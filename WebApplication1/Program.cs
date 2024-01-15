3d68fc0330fc74ed5c338d1d7420e465a17ae3bf
Maksim9056 <bobreczovm@inbox.ru>
2024-01-15 12:16:08 +0300

    Добавил Папку

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.Extensions.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.Run(async (context) =>
+//{
+//    var response = context.Response;
+//    response.ContentType = "text/html; charset=utf-8";
+//    await response.WriteAsync("<button>Hello METANIT.COM</button>");
+//});
+//app.MapGet("/", () => "Hello World!");
+app.Run(async (context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+
+    // если обращение идет по адресу "/postuser", получаем данные формы
+    if (context.Request.Path == "/postuser")
+    {
+        var form = context.Request.Form;
+        string name = form["name"];
+        string age = form["age"];
+        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
+    }
+    else
+    {
+        await context.Response.SendFileAsync("html/index.html");
+    }
+});
 
 app.Run();
